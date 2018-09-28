20a5efd3eb9138d759b6c70dce1ac658135f72b2
Matt Noyer <Matthew.noyer@kearsarge.org>
2018-09-28 07:51:20 -0400

    Maaaattttt

--- screens.py
+++ screens.py
@@ -4,7 +4,7 @@ def showSplash():
     output += "|                                     |\n"
     output += "|              MADLIBS                |\n"
     output += "|                 by                  |\n"
-    output += "|              Some Students          |\n"
+    output += "|              MATT          |\n"
     output += "|                                     |\n"
     output += "---------------------------------------\n"
 
