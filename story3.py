93eb2003390eb18b7d4fe4f61cac071d00c60c75
Matt Noyer <Matthew.noyer@kearsarge.org>
2018-10-10 07:59:38 -0400

    added to story 3

--- story3.py
+++ story3.py
@@ -4,9 +4,9 @@ from getInput import *
 def playMadlibs():
     adjective1 = getWord("Enter an adjective: ")
     celebrity1 = getWord("Enter a female celebrity name: ")
-    pet1 = getPet("Enter a common jungle ani animal")
-    food1 = getWord("Enter a type of unhealthy food")
-    
+    pet1 = getPet("Enter a common jungle ani animal: ")
+    food1 = getWord("Enter a type of unhealthy food: ")
+    god1 = getGod("Enter an olympian: "
 
 
 
@@ -15,4 +15,5 @@ Output+= "Once upon a time, there was a" + adjective1 + "princess by the name of
 Output+= celebrity1 + "lived high in a tower, protected night and day by her fathers massive" + pet1
 Output+= "One day, a brave prince attempted to rescue" + celebrity1 + "."
 Output+= "The prince charged the castle, wielding a" + food1 + "of unusual size"
-output+= "
+output+= "Out of nowhere, the princes's father, " + god1 + ", roundhouse kicked the charging prince."
+Output+= "
